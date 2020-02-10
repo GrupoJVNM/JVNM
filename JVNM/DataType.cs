35933ca47a313251d60de32443462d16e917d96b
georgerioboo <jorge97x92@gmail.com>
2020-02-10 13:27:37 +0100

    definition of classes

---
+++ JVNM/DataType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JVNM
+{
+    public enum DataType { Int, Double, Text };
+
+    
+
+
+}
---
+++ JVNM/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JVNM
+{
+    public class Database
+    {
+        public List<Table> tables;
+        public Database()
+        {
+            tables = new List<Table>();
+        }
+        public Database Load()
+        {
+            return null;
+        }
+    }
+}
