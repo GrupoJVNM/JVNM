9d2fca0e20525a4f0b38412379727089c1559ace
miren32 <miren_elizondo@hotmail.com>
2020-02-13 13:26:13 +0100

    create testTableColumn

---
+++ UnitTests/TestTableColumn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestTableColumn
+    {
+        List<DataType> dataList = new dataList() { Ana, Mario, Juan };
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            dataList.Add("Itxaso");
+            Assert.AreEquals();
+
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestUpdate()
+        {
+
+        }
+    }
+}
