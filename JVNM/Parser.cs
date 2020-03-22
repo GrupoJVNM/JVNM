d42250ef8b17009c98f69450a35628324d83bab6
Jorge Rodriguez <jorgerioboo@outlook.es>
2020-03-22 20:55:41 +0100

    create Parser class

---
+++ JVNM/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Parser
+{
+	public Parser()
+	{
+		 static Query ParseMiniSQL(string miniSQLQuery)
+		{
+			/*este método hace el parsing y devuelve la subclase de Query que corresponda con los parámetros de la sentencia
+		
+					table =
+			return new SelectAll(table)*/
+		}
+	}
+}
+
