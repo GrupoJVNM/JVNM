52a27125659157ce5dc363a927e3d2efa2c1701a
Jorge Rodriguez <jorgerioboo@outlook.es>
2020-04-13 17:44:18 +0200

    implement deleteUser

--- JVNM/DeleteUser.cs
+++ JVNM/DeleteUser.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace JVNM
 {
-   public class DeleteUser
+   public class DeleteUser : MiniSQLQuery
     {
-        DeleteUser() { 
-        
+        public string name;
+        public DeleteUser(string name)
+        {
+            this.name = name;
+        }
+
+        public string Execute(Database database)
+        {
+            // return database.DeleteUser(name);
+            throw new NotImplementedException();
         }
     }
 }
