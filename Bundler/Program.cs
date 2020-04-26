798f2488c2a8b2495eff8cbbf4b8727ddd81d527
Jorge Rodriguez <jorgerioboo@outlook.es>
2020-04-26 23:22:07 +0200

    adittion Bundler

---
+++ Bundler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bundler
+{
+    class Program
+    {
+        //this make automatic release 
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
